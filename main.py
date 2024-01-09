6d72ae84ab4b2c1476191f0fadca33d62aa4dd89
Nikita Staselovich <nikitastaselovich@gmail.com>
2024-01-09 14:23:05 +0300

    UPD: added time tracking script

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from time import sleep
-from user_actions import go_to_url
+import time
+
 import proxies
+from user_actions import go_to_url
 
-# for proxy in proxies.list:
-#     options = Options()
-#     options.add_argument(f"--proxy-server={proxy}")
-#     options.add_argument("--headless")
-#     browser = webdriver.Chrome(options=options)
-#     browser.set_page_load_timeout(5)
-#     try:
-#         browser.get("https://2ip.ru/")
-#         sleep(5)
-#     except Exception as e:
-#         (proxy, "is not working")
+start_time = time.time()
+working_proxy_count = 0
+nonworking_proxy_count = 0
+for proxy in proxies.list:
+    options = Options()
+    options.add_argument(f"--proxy-server={proxy}")
+    options.add_argument("--headless")
+    browser = webdriver.Chrome(options=options)
+    browser.set_page_load_timeout(5)
+    try:
+        go_to_url(browser)
+        print(proxy, " is working start to work")
+        working_proxy_count += 1
+        time.sleep(5)
+    except Exception as e:
+        working_proxy_count -= 1
 
-options = Options()
-# options.add_argument(f"--proxy-server={proxy}")
-# options.add_argument("--headless")
-browser = webdriver.Chrome(options=options)
-go_to_url(browser)
+elapsed_time = time.time() - start_time
+print(f"Script execution time: {elapsed_time} seconds")
+print(f"Working proxy count: {working_proxy_count}")
+print(f"Nonworking proxy count: {nonworking_proxy_count}")
--- user_actions.py
+++ user_actions.py
@@ -1,9 +1,9 @@
-from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
-
-from random import choice
+from time import sleep
+from random import choice, randint
 
 base_url = "https://tiny-chough.cloudvent.net/"
 passw = "rhjyjy?cjanm!"
@@ -19,63 +19,47 @@ def go_to_url(browser):
 
     all_links = browser.find_elements(By.TAG_NAME, "a")
     target_el = choice(all_links)
-
+    browser.execute_script("arguments[0].scrollIntoView();", target_el)
+    contact_us_actions(browser)
+    sleep(random())
     try:
         if target_el.get_attribute("class") == dropdown_items_class:
             dropdown_btn_class = "group.cursor-pointer.relative.rounded-md"
             btn = browser.find_element(By.CLASS_NAME, dropdown_btn_class)
             btn.click()
-            sleep(2)
-        target_el.click()
-        sleep(2)
-        push_page_down_btn(browser.find_element(By.TAG_NAME, "body"))
+            sleep(random())
+        sleep(random())
+        browser.execute_script("arguments[0].click();", target_el)
+        push_page_down_btn() if random() % 2 == 0 else contact_us_actions(browser)
 
     except WebDriverException as e:
-        child_els = target_el.find_elements(By.CSS_SELECTOR, "*")
+        print(e)
 
-        if not len(child_els):
-            [contact_us_actions(browser) for i in range(0, 2)]
-        else:
-            clicked_el = try_to_click_on_els(child_els)
-            push_page_down_btn(clicked_el)
-
-    sleep(50)
+    sleep(random())
 
 
-def try_to_click_on_els(els):
-    for el in els:
-        try:
-            el.click()
-            return el
-        except Exception as e:
-            print(el)
-            continue
-        finally:
-            sleep(2)
-
-
-def push_page_down_btn(body):
-    range_limit = range(0, 4)
+def push_page_down_btn(browser):
+    body = browser.find_element(By.TAG_NAME, "body")
+    range_limit = range(0, 5)
     limit = choice(range_limit)
     while limit:
         body.send_keys(Keys.PAGE_DOWN)
-        sleep(2)
+        sleep(random())
         if choice(range_limit) % 2 == 0:
             body.send_keys(Keys.PAGE_UP)
         limit -= 1
-        sleep(5)
+        sleep(random())
 
 
 def contact_us_actions(browser):
     inputs = [
-        get_el_by_tag(browser, "input"),
-        get_el_by_tag(browser, "input"),
-        get_el_by_tag(browser, "textarea"),
+        *browser.find_elements(By.XPATH, "//input"),
+        browser.find_element(By.XPATH, "//textarea"),
     ]
 
     random_input = choice(inputs)
-    random_input.click()
+    browser.execute_script("arguments[0].click();", random_input)
 
 
-def get_el_by_tag(browser, tag):
-    return browser.find_element(By.TAG_NAME, tag)
+def random():
+    return randint(0, 10)
